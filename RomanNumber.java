d22b9d4b462eda9104922e13178184fed6989b20
vagofrank <53983485+vagofrank@users.noreply.github.com>
2019-11-18 11:46:06 -0600

    Integer to Roman Number
    Added main.

--- RomanNumber.java
+++ RomanNumber.java
@@ -47,4 +47,14 @@ public class RomanNumber {
 
     }
 
+    // Run to test conversion from 1 to 3999 */
+    public static void main(String[] argv){
+        RomanNumber rm = new RomanNumber();
+
+        for(int i = 1; i <= 3999; i++){
+            System.out.println(i + "   -->   " + rm.IntegerToRomanNumber(i));
+        }
+
+    }
+   
 }
